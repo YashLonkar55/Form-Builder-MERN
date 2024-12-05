011ed73f812a1ab45374d0b87fef0181ea0ccc60
YashLonkar <yashlonkar99@gmail.com>
2024-12-05 16:54:31 +0530

    Final Commit OF From-BuilderV2

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { formRoutes } from './routes/formRoutes.js';
 import { responseRoutes } from './routes/responseRoutes.js';
-
+// import { fileURLToPath } from 'url';
+// import path from "path";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 // app.use(express.json());
@@ -19,13 +21,18 @@ app.use(express.urlencoded({ limit: '20mb', extended: true }));
 app.use('/api/forms', formRoutes);
 app.use('/api/responses', responseRoutes);
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
+
+// // Serve the index.html file for all other routes
+// app.get('*')
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
-
-console.log(process.env.MONGODB_URI);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
